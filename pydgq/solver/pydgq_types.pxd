75842f210cc3f4c18d43f545b45610620664e009
Juha Jeronen <juha.jeronen@jyu.fi>
2017-04-11 19:12:24 +0300

    add missing files pydgq_types.pyx,pxd

---
+++ pydgq/solver/pydgq_types.pxd
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+cimport numpy as cnp
+
+# real
+ctypedef cnp.float64_t DTYPE_t
+
+# complex
+ctypedef cnp.complex128_t DTYPEZ_t
+
---
+++ pydgq/solver/pydgq_types.pyx
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+from numpy import float64, complex128
+
+# real
+DTYPE = float64
+
+# complex
+DTYPEZ = complex128
+
