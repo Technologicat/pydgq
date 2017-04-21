b71cd60d2c9d42af6883fe21b9217f122d6bfd43
Juha Jeronen <juha.jeronen@jyu.fi>
2017-04-21 12:42:23 +0300

    improve tests

--- test/builtin_kernels_test.py
+++ test/builtin_kernels_test.py
@@ -14,20 +14,56 @@ import pydgq.solver.builtin_kernels
 import pydgq.solver.odesolve
 from pydgq.utils.discontify import discontify  # for plotting dG results
 
-integrator_to_test = "dG"
+
+#####################
+# config for testing
+#####################
+
 q = 2  # degree of basis for dG and cG
 
-nt = 10
-save_from = 0
-dt = 1.0  # dG can typically use huge timesteps
+# How many visualization (interpolation) points to use within each timestep for Galerkin methods.
+#
+# Note that the dG solution has the best accuracy at the endpoint of the timestep;
+# to compare apples-to-apples with classical integrators, this should be set to 1.
+#
+# Larger values (e.g. 11) are useful for visualizing the behavior of the dG solution inside
+# the timestep (something the classical integrators do not model at all).
+#
+nt_vis_galerkin = 1
 
-if integrator_to_test in ["dG", "cG"]:
-    nt_vis = 11  # visualization points per timestep in Galerkin methods
-else:
-    nt_vis = 1   # other methods compute only the end value for each timestep
+nt = 100   # number of timesteps
+dt = 0.02  # timestep size
+
+save_from = 0  # see pydgq.solver.odesolve.ivp()
 
 
-def test():
+# known analytical solution, for testing the integrators
+#
+# # see http://docs.sympy.org/dev/modules/solvers/ode.html
+# from sympy import Function, dsolve, Eq, Derivative, sin, cos, symbols
+# from sympy.abc import t
+# w = Function('w')
+# dsolve(Derivative(f(t), t) - f(t), f(t))
+#
+# ==> w(t) = C1 exp(t)
+#
+# where C1 = w(0) accounts for the initial condition.
+#
+def reference_solution(tt, n):  # n = number of DOFs
+    tt = np.atleast_1d(tt)
+    ww = np.empty( (tt.shape[0],n), dtype=DTYPE, order="C" )
+    for j in range(n):
+        w0 = float(j+1)
+        ww[:,j] = w0 * np.exp(tt)
+    return ww
+
+
+#####################
+# main program
+#####################
+
+# rel_tol : how close the numerical solution must be to the exact analytical one, in l-infinity norm (max abs)
+def test(integrator, nt_vis, rel_tol=1e-2, vis=False):
     n_saved_timesteps = pydgq.solver.odesolve.n_saved_timesteps( nt, save_from )
     result_len        = pydgq.solver.odesolve.result_len( nt, save_from, interp=nt_vis )
     startj,endj       = pydgq.solver.odesolve.timestep_boundaries( nt, save_from, interp=nt_vis )
@@ -45,37 +81,81 @@ def test():
     fail = np.empty( (n_saved_timesteps,), dtype=np.intc, order="C" )  # optional,  fail flag for each timestep, could be None
 
     # solve problem
-    ww,tt = pydgq.solver.odesolve.ivp( integrator=integrator_to_test, allow_denormals=False,
+    ww,tt = pydgq.solver.odesolve.ivp( integrator=integrator, allow_denormals=False,
                                        w0=w0, dt=dt, nt=nt,
                                        save_from=save_from, interp=nt_vis,
                                        rhs=rhs,
                                        ww=ww, ff=ff, fail=fail,
                                        maxit=100 )
 
-    # visualize
-    plt.figure(1)
-    plt.clf()
-
-    # show the discontinuities at timestep boundaries if using dG
-    if integrator_to_test == "dG":
-        tt = discontify( tt, endj - 1, fill="nan" )
-        for j in range(n):
-            # we need the copy() to get memory-contiguous data for discontify() to process
-            wtmp = discontify( ww[:,j].copy(), endj - 1, fill="nan" )
-            plt.plot( tt, wtmp )
+    # check result
+    ww_ref = reference_solution(tt, n)
+    relerr_linfty = np.linalg.norm(ww - ww_ref, ord=np.inf) / np.linalg.norm(ww_ref, ord=np.inf)
+    if (relerr_linfty < rel_tol).all():
+        passed = 1
+        if not vis:
+            print("PASS, %03s, tol=% 7g, relerr=%g" % (integrator, rel_tol, relerr_linfty))
     else:
-        plt.plot( tt, ww )
+        passed = 0
+        if not vis:
+            print("FAIL, %03s, tol=% 7g, relerr=%g" % (integrator, rel_tol, relerr_linfty))
 
-    plt.grid(b=True, which="both")
-    plt.axis("tight")
-    plt.xlabel(r"$t$")
-    plt.ylabel(r"$w(t)$")
+    # visualize if requested
+    if vis:
+        plt.figure(1)
+        plt.clf()
+
+        # show the discontinuities at timestep boundaries if using dG (and actually have something to draw within each timestep)
+        if integrator == "dG" and nt_vis > 1:
+            tt = discontify( tt, endj - 1, fill="nan" )
+            for j in range(n):
+                # we need the copy() to get memory-contiguous data for discontify() to process
+                wtmp = discontify( ww_ref[:,j].copy(), endj - 1, fill="nan" )
+                plt.plot( tt, wtmp, 'r--' )  # known exact solution
+
+                wtmp = discontify( ww[:,j].copy(), endj - 1, fill="nan" )
+                plt.plot( tt, wtmp, 'k-'  )  # numerical solution
+        else:
+            plt.plot( tt, ww_ref, 'r--' )  # known exact solution
+            plt.plot( tt, ww,     'k-'  )  # numerical solution
+
+        plt.grid(b=True, which="both")
+        plt.axis("tight")
+        plt.xlabel(r"$t$")
+        plt.ylabel(r"$w(t)$")
+
+    return passed
+
 
 if __name__ == '__main__':
-    # initialize Galerkin integrators
-    if integrator_to_test in ["dG", "cG"]:
-        init(q=q, method=integrator_to_test, nt_vis=nt_vis, rule=None)
+    print("** Testing integrators with 1st-order linear kernel **")
 
-    test()
+    # "SE" is not applicable, since we are testing a 1st-order problem
+    stuff_to_test = ( ("IMR", 1e-3,  False),
+                      ("BE",  1e-1,  False),
+                      ("RK4", 1e-8,  False),
+                      ("RK3", 1e-6,  False),
+                      ("RK2", 1e-3,  False),
+                      ("FE",  1e-1,  False),
+                      ("dG",  1e-12, True ),
+                      ("cG",  1e-4,  True )
+                    )
+    n_passed = 0
+
+    for integrator,rel_tol,is_galerkin in stuff_to_test:
+        if is_galerkin:
+            nt_vis = nt_vis_galerkin  # visualization points per timestep in Galerkin methods
+            init(q=q, method=integrator, nt_vis=nt_vis, rule=None)
+        else:
+            nt_vis = 1   # other methods compute only the end value for each timestep
+
+        n_passed += test(integrator, nt_vis, rel_tol)
+    print("** %d/%d tests passed **" % (n_passed, len(stuff_to_test)))
+
+    # draw something
+    #
+    nt_vis = nt_vis_galerkin
+    init(q=q, method="dG", nt_vis=nt_vis, rule=None)
+    test(integrator="dG", nt_vis=nt_vis, vis=True)
     plt.show()
 
--- test/python_kernel_test.py
+++ test/python_kernel_test.py
@@ -15,6 +15,32 @@ import pydgq.solver.odesolve
 from pydgq.utils.discontify import discontify  # for plotting dG results
 
 
+#####################
+# config for testing
+#####################
+
+q = 2  # degree of basis for dG and cG
+
+# How many visualization (interpolation) points to use within each timestep for Galerkin methods.
+#
+# Note that the dG solution has the best accuracy at the endpoint of the timestep;
+# to compare apples-to-apples with classical integrators, this should be set to 1.
+#
+# Larger values (e.g. 11) are useful for visualizing the behavior of the dG solution inside
+# the timestep (something the classical integrators do not model at all).
+#
+nt_vis_galerkin = 1
+
+nt = 100  # number of timesteps
+dt = 0.1  # timestep size
+
+save_from = 0  # see pydgq.solver.odesolve.ivp()
+
+
+#####################
+# custom kernel
+#####################
+
 # A simple cosine kernel with phase-shifted components.
 #
 # The custom kernel only needs to override callback(); even __init__ is not strictly needed,
@@ -33,21 +59,24 @@ class MyKernel(PythonKernel):
             phi0_j = (float(j+1) / self.n) * 2. * np.pi
             self.out[j] = np.cos(phi0_j + self.omega*t)
 
-
-integrator_to_test = "dG"
-q = 2  # degree of basis for dG and cG
-
-nt = 10
-save_from = 0
-dt = 1.0  # dG can typically use huge timesteps
-
-if integrator_to_test in ["dG", "cG"]:
-    nt_vis = 11  # visualization points per timestep in Galerkin methods
-else:
-    nt_vis = 1   # other methods compute only the end value for each timestep
+    # known analytical solution, for testing the integrators
+    #
+    def reference_solution(self, tt):
+        tt = np.atleast_1d(tt)
+        ww = np.empty( (tt.shape[0],self.n), dtype=DTYPE, order="C" )
+        for j in range(self.n):
+            phi0_j  = (float(j+1) / self.n) * 2. * np.pi
+            sol     = lambda t : 1./self.omega * np.sin(phi0_j + self.omega*t)
+            ww[:,j] = sol(tt) - sol(0.)  # shift to account for the initial condition (all solution components start at zero)
+        return ww
 
 
-def test():
+#####################
+# main program
+#####################
+
+# rel_tol : how close the numerical solution must be to the exact analytical one, in l-infinity norm (max abs)
+def test(integrator, nt_vis, rel_tol=1e-2, vis=False):
     n_saved_timesteps = pydgq.solver.odesolve.n_saved_timesteps( nt, save_from )
     result_len        = pydgq.solver.odesolve.result_len( nt, save_from, interp=nt_vis )
     startj,endj       = pydgq.solver.odesolve.timestep_boundaries( nt, save_from, interp=nt_vis )
@@ -64,37 +93,81 @@ def test():
     fail = np.empty( (n_saved_timesteps,), dtype=np.intc, order="C" )  # optional,  fail flag for each timestep, could be None
 
     # solve problem
-    ww,tt = pydgq.solver.odesolve.ivp( integrator=integrator_to_test, allow_denormals=False,
+    ww,tt = pydgq.solver.odesolve.ivp( integrator=integrator, allow_denormals=False,
                                        w0=w0, dt=dt, nt=nt,
                                        save_from=save_from, interp=nt_vis,
                                        rhs=rhs,
                                        ww=ww, ff=ff, fail=fail,
                                        maxit=100 )
 
-    # visualize
-    plt.figure(1)
-    plt.clf()
-
-    # show the discontinuities at timestep boundaries if using dG
-    if integrator_to_test == "dG":
-        tt = discontify( tt, endj - 1, fill="nan" )
-        for j in range(n):
-            # we need the copy() to get memory-contiguous data for discontify() to process
-            wtmp = discontify( ww[:,j].copy(), endj - 1, fill="nan" )
-            plt.plot( tt, wtmp )
+    # check result
+    ww_ref = rhs.reference_solution(tt)
+    relerr_linfty = np.linalg.norm(ww - ww_ref, ord=np.inf) / np.linalg.norm(ww_ref, ord=np.inf)
+    if (relerr_linfty < rel_tol).all():
+        passed = 1
+        if not vis:
+            print("PASS, %03s, tol=% 7g, relerr=%g" % (integrator, rel_tol, relerr_linfty))
     else:
-        plt.plot( tt, ww )
+        passed = 0
+        if not vis:
+            print("FAIL, %03s, tol=% 7g, relerr=%g" % (integrator, rel_tol, relerr_linfty))
 
-    plt.grid(b=True, which="both")
-    plt.axis("tight")
-    plt.xlabel(r"$t$")
-    plt.ylabel(r"$w(t)$")
+    # visualize if requested
+    if vis:
+        plt.figure(1)
+        plt.clf()
+
+        # show the discontinuities at timestep boundaries if using dG (and actually have something to draw within each timestep)
+        if integrator == "dG" and nt_vis > 1:
+            tt = discontify( tt, endj - 1, fill="nan" )
+            for j in range(n):
+                # we need the copy() to get memory-contiguous data for discontify() to process
+                wtmp = discontify( ww_ref[:,j].copy(), endj - 1, fill="nan" )
+                plt.plot( tt, wtmp, 'r--' )  # known exact solution
+
+                wtmp = discontify( ww[:,j].copy(), endj - 1, fill="nan" )
+                plt.plot( tt, wtmp, 'k-'  )  # numerical solution
+        else:
+            plt.plot( tt, ww_ref, 'r--' )  # known exact solution
+            plt.plot( tt, ww,     'k-'  )  # numerical solution
+
+        plt.grid(b=True, which="both")
+        plt.axis("tight")
+        plt.xlabel(r"$t$")
+        plt.ylabel(r"$w(t)$")
+
+    return passed
+
 
 if __name__ == '__main__':
-    # initialize Galerkin integrators
-    if integrator_to_test in ["dG", "cG"]:
-        init(q=q, method=integrator_to_test, nt_vis=nt_vis, rule=None)
+    print("** Testing integrators with custom kernel **")
 
-    test()
+    # "SE" is not applicable, since we are testing a 1st-order problem
+    stuff_to_test = ( ("IMR", 1e-3,  False),
+                      ("BE",  1e-1,  False),
+                      ("RK4", 1e-8,  False),
+                      ("RK3", 1e-6,  False),
+                      ("RK2", 1e-3,  False),
+                      ("FE",  1e-1,  False),
+                      ("dG",  1e-12, True ),
+                      ("cG",  1e-4,  True )
+                    )
+    n_passed = 0
+
+    for integrator,rel_tol,is_galerkin in stuff_to_test:
+        if is_galerkin:
+            nt_vis = nt_vis_galerkin  # visualization points per timestep in Galerkin methods
+            init(q=q, method=integrator, nt_vis=nt_vis, rule=None)
+        else:
+            nt_vis = 1   # other methods compute only the end value for each timestep
+
+        n_passed += test(integrator, nt_vis, rel_tol)
+    print("** %d/%d tests passed **" % (n_passed, len(stuff_to_test)))
+
+    # draw something
+    #
+    nt_vis = nt_vis_galerkin
+    init(q=q, method="dG", nt_vis=nt_vis, rule=None)
+    test(integrator="dG", nt_vis=nt_vis, vis=True)
     plt.show()
 
